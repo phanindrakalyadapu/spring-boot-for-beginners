c0de92599e7a1c3c92119c02f619b4a39626ca0c
phanindra <phanindrakalyadapu2611@gmail.com>
2023-03-17 17:54:29 -0600

    application

---
+++ src/test/java/com/phani/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phani.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
